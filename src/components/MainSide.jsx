f50c794c95097c803c658832dcca0fe7a2cdfb91
Mehebul02 <mehebul2123@gmail.com>
2024-11-21 22:42:16 +0600

    added the icon my website

--- src/components/MainSide.jsx
+++ src/components/MainSide.jsx
@@ -1,7 +1,10 @@
 import { duacard } from "@/assets/images";
 import Image from "next/image";
 import React from "react";
-
+import { CiBookmark } from "react-icons/ci";
+import { FaRegLightbulb } from "react-icons/fa";
+import { MdOutlineContentCopy, MdReportGmailerrorred } from "react-icons/md";
+import { IoShareSocialOutline } from "react-icons/io5";
 const MainSide = () => {
   return (
     <div>
@@ -40,13 +43,27 @@ const MainSide = () => {
       {/* category  */}
 
       <div className="mt-10  bg-white w-full p-5 rounded-md border">
-        <div className="flex items-center gap-4 text-[12px] font-medium font-inter text-black text-primary-color">
-          <Image src={duacard} alt="dua"/> 
+        <div className="flex items-center gap-4 text-[12px] font-medium font-inter  text-primary-color">
+          <Image src={duacard} alt="dua" />
           1. The servant is dependent on his Lord #1
         </div>
 
-        <h1 className="text-[13px] text-[#445666] font-inter font-normal mt-3 space-y-1">All human beings depend on Allah for their welfare and prevention of evil in various matters of their religion and world. Allah says (interpretation of the meaning): O mankind, you are those in need of Allah, while Allah is the Free of need, the Praiseworthy.</h1>
-
+        <h1 className="text-[13px] text-[#445666] font-inter font-normal mt-3 space-y-1">
+          All human beings depend on Allah for their welfare and prevention of
+          evil in various matters of their religion and world. Allah says
+          (interpretation of the meaning): O mankind, you are those in need of
+          Allah, while Allah is the Free of need, the Praiseworthy.
+        </h1>
+        <h1 className="text-[12px] mt-2 font-inter text-primary-color">Reference:</h1>
+        <h1 className="text-[12px] mt-2 font-inter text-black font-medium">Surah Al-Fatir 35:15</h1>
+        {/* icon  */}
+        <div className="flex gap-5 justify-end cursor-pointer">
+            <span> <MdOutlineContentCopy /> </span>
+            <span> <CiBookmark/> </span>
+            <span> <FaRegLightbulb/> </span>
+            <span> <IoShareSocialOutline/> </span>
+            <span> <MdReportGmailerrorred/> </span>
+        </div>
       </div>
     </div>
   );
