82508ee0b421429d7f11eea18dd588144fb3b5cd
Mehebul02 <mehebul2123@gmail.com>
2024-11-21 19:50:53 +0600

    added the dark icon

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,5 +5,6 @@ import dua_kobuler from '../images/dua_kobuler_somoy.svg'
 import jader_dua from '../images/jader_dua_kobul_hoy.svg'
 import profile from '../images/profile.svg'
 import language from '../images/language.svg'
+import general from '../images/general.svg'
 
-export  {dui,zikirer,dua_kobuler,jader_dua,profile,language}
+export  {dui,zikirer,dua_kobuler,jader_dua,profile,language,general}
--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -1,4 +1,4 @@
-import { language, profile } from "@/assets/images";
+import { general, language, profile } from "@/assets/images";
 import Image from "next/image";
 import React from "react";
 import { FaHandHoldingHeart } from "react-icons/fa";
@@ -58,30 +58,91 @@ const LeftSide = () => {
         <h1 className="text-xl text-black text-center font-inter font-medium p-4">
           Settings
         </h1>
+        <div className="gap-0">
+          {/* 1st title  */}
+          <div className="collapse ">
+            <input type="checkbox" />
+            <div className="collapse-title  ">
+              <div className="flex flex-row justify-between items-center  bg-[#f9f9f9]  rounded-md  w-full">
+                <div className="flex justify-center items-center gap-5">
+                  <Image
+                    src={language}
+                    alt="dui"
+                    className="bg-[#EBEEF2] w-11 h-11 p-4 rounded-full "
+                  />
 
-        <div className="collapse ">
-          <input type="checkbox" />
-          <div className="collapse-title  ">
-            <div className="flex flex-row justify-between items-center  bg-[#f9f9f9] p-2 rounded-md  w-full">
-              <div className="flex justify-center items-center gap-5">
-                <Image
-                  src={language}
-                  alt="dui"
-                  className="bg-[#EBEEF2] w-11 h-11 p-4 rounded-full "
-                />
-
-                <div className="">
-                  <h1 className="text-[12px] text-black font-inter font-semibold">
-                    Language Settings
-                  </h1>
+                  <div className="">
+                    <h1 className="text-[12px] text-black font-inter font-semibold">
+                      Language Settings
+                    </h1>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="collapse-content">
+              <div className=" space-x-10 flex ">
+                <button className="text-sm text-white  pl-4 pr-4 pt-1 pb-1 rounded-sm font-inter bg-primary-color">
+                  English
+                </button>
+                <button className="text-sm text-black  pl-4 pr-4 pt-1 pb-1 rounded-sm font-inter border border-primary-color">
+                  English
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="collapse-content">
-            <div className=" space-x-10 flex justify-center">
-            <button className="text-sm text-white  pl-4 pr-4 pt-1 pb-1 rounded-sm font-inter bg-primary-color">English</button>
-            <button className="text-sm text-black  pl-4 pr-4 pt-1 pb-1 rounded-sm font-inter border border-primary-color">English</button>
+          {/* 2nd title  */}
+          <div className="collapse ">
+            <input type="checkbox" />
+            <div className="collapse-title  ">
+              <div className="flex flex-row justify-between items-center  bg-[#f9f9f9] p-2 rounded-md  w-full">
+                <div className="flex justify-center items-center gap-5">
+                  <Image
+                    src={general}
+                    alt="dui"
+                    className="bg-[#EBEEF2] w-11 h-11 p-4 rounded-full "
+                  />
+
+                  <div className="">
+                    <h1 className="text-[12px] text-black font-inter font-semibold">
+                      General Settings
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="collapse-content"></div>
+          </div>
+          {/* 3rd title  */}
+          <div className="collapse ">
+            <input type="checkbox" />
+            <div className="collapse-title  ">
+              <div className="flex flex-row justify-between bg-[#f9f9f9] p-2 rounded-md  w-full">
+                <div className="flex justify-center items-center gap-5">
+                  <Image
+                    src={general}
+                    alt="dui"
+                    className="bg-[#EBEEF2] w-11 h-11 p-4 rounded-full "
+                  />
+
+                  <div className="">
+                    <h1 className="text-[12px] text-black font-inter font-semibold">
+                      General Settings
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="collapse-content">
+              <div className=" space-x-10 flex justify-center ">
+                <h3 className="text-[12px] text-[#201d20] font-semibold">
+                  Night Mode
+                </h3>
+                <input
+                  type="checkbox"
+                  value="synthwave"
+                  className="toggle theme-controller col-span-1 col-start-1 row-start-1 border-gray-500 bg-gray-300 [--tglbg:theme(colors.gray.500)] checked:border-gray-800 checked:bg-gray-700 checked:[--tglbg:theme(colors.gray.900)]"
+                />
+              </div>
             </div>
           </div>
         </div>
