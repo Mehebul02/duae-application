135a7bec5bfa687b0a95dbafebefc3c501cbcd54
Mehebul02 <mehebul2123@gmail.com>
2024-11-22 09:38:19 +0600

    added favicon icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-          <link rel="icon" href="/favicon.svg" />
+          <link rel="icon" href="/favicon.ico" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
